538a2f78fac043272660085f2e89a0170068a6a1
PUSSSO <paul.robalino@udla.edu.ec>
2025-01-26 17:16:06 -0500

    feat:template for sending the message

---
+++ src/main/java/com/udla/models/MessageModel.java
@@ -0,0 +1,16 @@
+package com.udla.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageModel {
+    private String idClient;
+    private String name;
+    private String productName;
+    private int productQuantity;
+    private String secondIdClient;
+}
